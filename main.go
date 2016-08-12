c91c9fd13562264ec1752e44bdb8cf322d7607da
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-08-13 04:13:52 +0900

    Fix

--- main.go
+++ main.go
@@ -9,23 +9,6 @@ import (
 var port string
 
 func main() {
-	/*
-		client := createClient()
-		bucketName := "yuyat-s3-sandbox"
-		params := &s3.ListObjectsInput{
-			Bucket: &bucketName,
-		}
-		err := client.ListObjectsPages(params, func(p *s3.ListObjectsOutput, lastPage bool) bool {
-			pp.Println(p)
-			pp.Println(lastPage)
-
-			return true
-		})
-		if err != nil {
-			panic(err)
-		}
-	*/
-
 	envPort := os.Getenv("PORT")
 	if envPort != "" {
 		port = envPort
@@ -37,6 +20,7 @@ func main() {
 
 	handle("/ping", handlePing)
 	handle("/upload.cgi", routeByMethods(methodHandlerMap{"POST": handleUpload}))
+
 	fmt.Printf("Listening on %s...\n", addr)
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
